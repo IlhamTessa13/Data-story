bb6e4f6f524d7cd6a98935b3118d02ce84778b04
IlhamTessa13 <ilhamts139@gmail.com>
2026-04-14 12:13:12 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="bg-white text-gray-900">
+      <h1 className="text-4xl font-bold p-8">
+        Data Story — Kemiskinan Indonesia
+      </h1>
+    </main>
+  );
+}
+
+export default App;
